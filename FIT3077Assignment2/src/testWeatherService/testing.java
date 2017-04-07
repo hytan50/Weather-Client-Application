3435888ea1e29d1d631c5127296ff0be09cba777
Saurav Kadam <skadam2996@gmail.com>
2017-04-07 18:14:03 +1000

    hahahahahha

--- FIT3077Assignment2/src/testWeatherService/testing.java
+++ FIT3077Assignment2/src/testWeatherService/testing.java
@@ -5,7 +5,7 @@ import java.lang.*;
 public class testing {
 
 	public static void main(String[] args) {
-		System.out.println("hahhahahahahahahhahahah!");
+		System.out.println("hahahahhahahahah!");
 
 	}
 
