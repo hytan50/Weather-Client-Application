45c0467db8aba4d6391ddb4937b7a53d99ce3de5
hytan50 <hytan50@gmail.com>
2017-04-19 14:33:01 +1000

    created a new package GUI and a file named homeScreen

---
+++ FIT3077Assignment2/src/GUI/homeScreen.java
@@ -0,0 +1,43 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class homeScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					homeScreen window = new homeScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public homeScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
