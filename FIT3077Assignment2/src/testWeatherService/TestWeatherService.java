0d6c4c7966cd4c39c7f529202727e03a1eb0ada0
Saurav Kadam <skadam2996@gmail.com>
2017-04-21 15:38:01 +1000

    Modified TestWeatherService
    Removed the main method, replaced it with two other methods that return
    the temperature and the rainfall for a specific location using index.

--- FIT3077Assignment2/src/testWeatherService/TestWeatherService.java
+++ FIT3077Assignment2/src/testWeatherService/TestWeatherService.java
@@ -25,67 +25,40 @@ public class TestWeatherService extends Timer{
 	private static String[] lol = new String[40];
 	private static final int TemperatureIndex = 1;
 	String[] output = new String[200];
-	
-	
-	public static void main(String[] args) throws Exception {
 
+	public String getTemp(int index) throws Exception{
 		final MelbourneWeather2Stub MelbourneWeatherService = new MelbourneWeather2Stub();
 		
 		// Get the available locations from the web service
 		GetLocationsResponse LocationsResponse = MelbourneWeatherService.getLocations();
+		String[] Locations = LocationsResponse.get_return();
+		String[] Temperature = new String[Locations.length];
+		GetTemperature TemperatureRequest = new GetTemperature();
+		TemperatureRequest.setLocation(Locations[index-1]);
+		GetTemperatureResponse TemperatureResponse = MelbourneWeatherService.getTemperature(TemperatureRequest);
+		lol = TemperatureResponse.get_return();
+		String temp = lol[TemperatureIndex];
+		return temp;
+	}
+	
+	public String getRain(int index) throws Exception{
+		final MelbourneWeather2Stub MelbourneWeatherService = new MelbourneWeather2Stub();
 		
-		Scanner input = new Scanner(System.in);  // Reading from System.in
-		
+		// Get the available locations from the web service
+		GetLocationsResponse LocationsResponse = MelbourneWeatherService.getLocations();
 		String[] Locations = LocationsResponse.get_return();
 		String[] Rainfall = new String[2];
 		String[] Rainfalls1 = new String[Locations.length];
 		String[] Rainfalls2 = new String[Locations.length];
-		String[] Temperature = new String[Locations.length];
-		int n;
-		
-		System.out.println("Enter a location number: [1-22], because we currently have 22 locations ");
-		n = input.nextInt();
 		GetRainfall RainfallRequest = new GetRainfall();
-		RainfallRequest.setLocation(Locations[n-1]);
+		RainfallRequest.setLocation(Locations[index-1]);
 		GetRainfallResponse RainfallResponse = MelbourneWeatherService.getRainfall(RainfallRequest);
 		Rainfall = RainfallResponse.get_return();
-		Rainfalls1[n-1] = Rainfall[TimestampIndex];
-		Rainfalls2[n-1] = Rainfall[RainfallIndex];
-		GetTemperature TemperatureRequest = new GetTemperature();
-		TemperatureRequest.setLocation(Locations[n-1]);
-		GetTemperatureResponse TemperatureResponse = MelbourneWeatherService.getTemperature(TemperatureRequest);
-		lol = TemperatureResponse.get_return();
-		String temp = lol[TemperatureIndex];
-		String rain1 = Rainfalls1[n-1];
-		String rain2 = Rainfalls2[n-1];
-		String locations = Locations[n-1];
-		
-		
-		Output a = new Output(locations,rain1,temp,rain2);
-		a.out();
-		//Timer timer = new Timer();
-		//timer.schedule(run(Locations,n,Rainfalls2, Temperature, Rainfalls1), 0, 5000);
-		/*
-		timer.schedule(new TimerTask() {
-		    public void run() {
-		       System.out.println(
-						Locations[n-1]
-								+ " @ " + Rainfalls1[n-1]
-								+ ":\n\tTemperature:\t" + lol[TemperatureIndex]
-								+ "\n\tRainfall:\t" + Rainfalls2[n-1]
-								+ "\n\n"
-							);
-		    }
-		}, 0, 300000);
-		
-		*/
-		
-		
-		
-		
-		// Loop over the locations, and display the temperature and rainfall at each
-		
-		
+		Rainfalls1[index-1] = Rainfall[TimestampIndex];
+		Rainfalls2[index-1] = Rainfall[RainfallIndex];
+		String rain1 = Rainfalls1[index-1];
+		String rain2 = Rainfalls2[index-1];
+		return rain2;
 	}
 
 }
