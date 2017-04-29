5dacd2e6835bd1ce09af7734a927ecbff4e7251a
hytan50 <hytan50@gmail.com>
2017-04-29 12:53:15 +1000

    refine the abstract homeScreen class and concreteHomeScreen class

--- FIT3077Assignment2/src/GUI/concreteHomeScreen.java
+++ FIT3077Assignment2/src/GUI/concreteHomeScreen.java
@@ -1,18 +1,21 @@
 package GUI;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import javax.swing.DefaultComboBoxModel;
+import melbourneweather2.MelbourneWeather2Stub;
+import melbourneweather2.MelbourneWeather2Stub.GetLocationsResponse;
 
 public class concreteHomeScreen extends homeScreen{
 
-	private JFrame frmMelbourneWeather;
 	
 	public concreteHomeScreen() throws Exception {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -24,6 +27,35 @@ public class concreteHomeScreen extends homeScreen{
 				}
 			}
 		});
+	}
+	
+	
+	@Override
+	public void showButtonClicked(){
+		
+		String selectedLocation = locationComboBox.getSelectedItem().toString();
+		int locationIndex = locationComboBox.getSelectedIndex();
+		if(!rainCheckbox.getState() && !tempCheckbox.getState()){}
+		else{
+		monitor newMonitor = new monitor(selectedLocation, locationIndex, tempCheckbox.getState(), rainCheckbox.getState());
+		newMonitor.frmMelbourneWeather.setVisible(true);
+		}
+		 
+	}
+	
+	@Override
+	public void addLocationToComboBox() throws Exception{
+		final MelbourneWeather2Stub MelbourneWeatherService = new MelbourneWeather2Stub();
+		GetLocationsResponse LocationsResponse = MelbourneWeatherService.getLocations();
+		
+		String[] locationList = LocationsResponse.get_return();
+		final DefaultComboBoxModel<Object> model = new DefaultComboBoxModel<Object>(locationList);
+		locationComboBox.setModel(model);
+	}
+	
+	@Override
+	public void setApplicationTitle(){
+		frmMelbourneWeather.setTitle("Melbourne Weather 2 Desktop Application");
 	}
 
 }
--- FIT3077Assignment2/src/GUI/homeScreen.java
+++ FIT3077Assignment2/src/GUI/homeScreen.java
@@ -1,41 +1,26 @@
 package GUI;
 
-//import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
-//import java.awt.event.ActionListener;
-//import java.awt.event.ActionEvent;
-//import GUI.monitor;
-import melbourneweather2.*;
-import melbourneweather2.MelbourneWeather2Stub.GetLocationsResponse;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.Checkbox;
 
 public abstract class homeScreen {
 
 	JFrame frmMelbourneWeather;
 	JButton showButton = new JButton("");
-	/**
-	 * Launch the application.
-	 */
+	Checkbox rainCheckbox = new Checkbox("Rainfall");
+	Checkbox tempCheckbox = new Checkbox("Temperature");
+	JComboBox<Object> locationComboBox = new JComboBox<Object>();
+	
+	
 	/*
 	 * main method is included in the concrete class, not abstract class
 	 * 
-	 * 
-	 * public static void main(String[] args) {
-	 *	EventQueue.invokeLater(new Runnable() {
-	 *	 	public void run() {
-	 *			try {
-	 *				homeScreen window = new homeScreen();
-	 *				window.frmMelbourneWeather.setVisible(true);
-	 *			} catch (Exception e) {
-	 *				e.printStackTrace();
-	 *			}
-	 *		}
-	 *	});
-	 *}
 	 */
-	/**
+	/*
 	 * Create the application.
 	 * @throws Exception 
 	 */
@@ -46,48 +31,39 @@ public abstract class homeScreen {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() throws Exception {
+	public void initialize() throws Exception {
 		
-		final MelbourneWeather2Stub MelbourneWeatherService = new MelbourneWeather2Stub();
-		GetLocationsResponse LocationsResponse = MelbourneWeatherService.getLocations();
-		
-		String[] locationList = LocationsResponse.get_return();
+		//main frame
 		frmMelbourneWeather = new JFrame();
 		frmMelbourneWeather.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frmMelbourneWeather.setTitle("Melbourne Weather 2 Desktop Application");
+		setApplicationTitle();
 		frmMelbourneWeather.setBounds(100, 100, 450, 300);
 		frmMelbourneWeather.getContentPane().setLayout(null);
 		
-		JComboBox<Object> locationComboBox = new JComboBox<Object>(locationList);
+		//JComboBox
 		locationComboBox.setBounds(117, 49, 147, 20);
 		frmMelbourneWeather.getContentPane().add(locationComboBox);
+		addLocationToComboBox();
 		
-		Checkbox tempCheckbox = new Checkbox("Temperature");
+		//tempCheckbox
 		tempCheckbox.setBounds(141, 116, 95, 22);
 		frmMelbourneWeather.getContentPane().add(tempCheckbox);
 		
-		Checkbox rainCheckbox = new Checkbox("Rainfall");
+		//rainCheckbox
 		rainCheckbox.setBounds(141, 161, 95, 22);
 		frmMelbourneWeather.getContentPane().add(rainCheckbox);
 		
 		
-		//JButton showButton = new JButton("Show Weather");
+		//showButton
 		showButton.setText("Show Weather");
-		/*
+		
 		  showButton.addActionListener(new ActionListener() {
 		 
 			public void actionPerformed(ActionEvent arg0) {
-				String selectedLocation = locationComboBox.getSelectedItem().toString();
-				int locationIndex = locationComboBox.getSelectedIndex();
-				if(!rainCheckbox.getState() && !tempCheckbox.getState()){}
-				else{
-				monitor newMonitor = new monitor(selectedLocation, locationIndex, tempCheckbox.getState(), rainCheckbox.getState());
-				newMonitor.frmMelbourneWeather.setVisible(true);
-				}
-				
+				showButtonClicked();
 			}
+			
 		});
-		*/
 		showButton.setBounds(117, 227, 147, 23);
 		frmMelbourneWeather.getContentPane().add(showButton);
 		
@@ -95,5 +71,12 @@ public abstract class homeScreen {
 		
 	}
 	
+	//showButton action Listener
 	public void showButtonClicked(){}
+	
+	//add location list to combo box
+	public void addLocationToComboBox() throws Exception{}
+	
+	//set Application name
+	public void setApplicationTitle(){}
 }
